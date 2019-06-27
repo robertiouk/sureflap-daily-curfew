1ff0c021c0aa18959c063913a64d420ef735e3e5
robertiouk <robertiouk@gmail.com>
2019-06-27 20:45:54 +0100

    Added fix for daylight saving times

--- daylight_util.py
+++ daylight_util.py
@@ -1,6 +1,8 @@
+import time
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import tzlocal
 
 _FORMAT = '%I:%M:%S %p'
 _URL = 'https://api.sunrise-sunset.org/json?lat=52.150492&lng=-0.545676&date={}'
@@ -16,7 +18,21 @@ def get_curfew():
     today = datetime.today()
     dates = _get_json(_URL.format(today.isoformat()))
 
+    localtime = time.localtime()
+    
+    hours_offset = 0
+    if localtime.tm_isdst == 1:
+        hours_offset = 1
+    delta = timedelta(hours = hours_offset)
+
+    print(delta)
+    
     lock = datetime.strptime(dates[3], _FORMAT)
     unlock = datetime.strptime(dates[2], _FORMAT)
+    lock = lock + delta
+    unlock = unlock + delta
+
+    print(lock)
+    print(unlock)
 
     return lock, unlock
